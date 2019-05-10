cb1328ea6c050fbf19e2a2496ccac0010b93ef38
Valentin Podkamennyi <valentin@dtm.io>
2019-05-09 18:30:55 -0700

    Initial version of Ananace frontend app.

--- build/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# Runs closure linter, closure compiler and closure stylesheets.
-# Link: https://github.com/google/closure-linter
-# Link: https://github.com/google/closure-compiler
-# Link: https://github.com/google/closure-stylesheets
-
-./jslint.sh && ./jsmin.sh && ./cssmin.sh && ./makeapp.sh
