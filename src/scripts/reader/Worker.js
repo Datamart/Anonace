514ff863a4d60db9697543335cdfb71a2ec58161
Valentin Podkamennyi <vpodk@users.noreply.github.com>
2021-03-31 23:22:58 -0700

    Update Worker.js

--- src/scripts/reader/Worker.js
+++ src/scripts/reader/Worker.js
@@ -1,81 +1,122 @@
-/**
- * @fileoverview Simple implementation of web worker service.
- *
- * @see http://google.github.io/styleguide/javascriptguide.xml
- * @see http://developers.google.com/closure/compiler/docs/js-for-compiler
- */
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en"
+      itemscope itemtype="http://schema.org/WebApplication">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=5">
+<meta name="google" content="notranslate">
+<!-- html markup -->
+<title>AnonAce - Follow and read favorite authors, topics, and hashtags.</title>
+<link rel="canonical" href="https://anonace.com/">
+<link rel="publisher" href="https://google.com/+DatamartInc" title="Google+">
+<meta name="description" content="Read your favorite authors, influencers and brands, follow your interests by topics and hashtags. Free. Anonymously. Securely. No sign-up. No ads.">
+<!--  schema.org markup -->
+<meta itemprop="name" content="AnonAce - Follow and read favorite authors, topics, and hashtags.">
+<meta itemprop="url" content="https://anonace.com/">
+<meta itemprop="description" content="Read your favorite authors, influencers and brands, follow your interests by topics and hashtags. Free. Anonymously. Securely. No sign-up. No ads.">
+<meta itemprop="image" content="https://anonace.com/images/feature-graphic-1024x500.jpg">
+<meta itemprop="applicationCategory" content="Social">
+<meta itemprop="operatingSystem" content="Any">
+<!--  open graph markup -->
+<meta property="og:title" content="AnonAce - Follow and read favorite authors, topics, and hashtags.">
+<meta property="og:url" content="https://anonace.com/">
+<meta property="og:description" content="Read your favorite authors, influencers and brands, follow your interests by topics and hashtags. Free. Anonymously. Securely. No sign-up. No ads.">
+<meta property="og:image" content="https://anonace.com/images/feature-graphic-1024x500.jpg">
+<meta property="og:type" content="website">
+<!--  twitter card markup -->
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:site" content="@thedatamartinc">
+<meta name="twitter:creator" content="@thedatamartinc">
+<meta name="twitter:title" content="AnonAce - Follow and read favorite authors, topics, and hashtags.">
+<meta name="twitter:url" content="https://anonace.com/">
+<meta name="twitter:description" content="Read your favorite authors, influencers and brands, follow your interests by topics and hashtags. Free. Anonymously. Securely. No sign-up. No ads.">
+<meta name="twitter:image" content="https://anonace.com/images/feature-graphic-1024x500.jpg">
+<meta name="twitter:app:name:googleplay" content="AnonAce">
+<meta name="twitter:app:id:googleplay" content="com.anonace.android">
+<meta name="twitter:app:url:googleplay" content="https://play.google.com/store/apps/details?id=com.anonace.android">
+<!-- end meta markup -->
+<meta name="theme-color" content="#222222">
+<meta name="mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-title" content="AnonAce">
+<meta name="application-name" content="AnonAce">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="./images/logo-144x144.png">
+<meta name="msapplication-config" content="none">
+<link rel="manifest" href="./manifest.json">
+<link rel="apple-touch-icon" sizes="180x180" href="./images/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="./images/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="./images/favicon-16x16.png">
+<link rel="shortcut icon" href="./favicon.ico">
+<link rel="mask-icon" href="./images/logo.svg" sizes="any" color="#444444">
+<style>
+html,body,#pwa-install,#not-supported,#ios-message{height:100%;margin:0;padding:0}
+#pwa-install,#not-supported,#ios-message{align-items:center;display:none;justify-content:center}
+#pwa-install span{color:blue;cursor:pointer;text-decoration:underline}
+</style>
+</head>
+<body>
+<div id="pwa-install"><span>Install App</span></div>
+<div id="not-supported">PWA installation is not supported.</div>
+<div id="ios-message">
+  <ol>
+    <li>
+      Click share button
+      <img height="22" alt="Share Button Image" src="data:image/svg+xml;base64,PHN2ZyBpZD0iTGF5ZXJfMiIgZGF0YS1uYW1lPSJMYXllciAyIiB4bWxucz0iaHR0cDovL3d3dy5 3My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyMC44OCAyNy4yNSI+PGRlZnM+PHN0eWxlPi 5jbHMtMXtmaWxsOm5vbmU7c3Ryb2tlOiMyMzFmMjA7fTwvc3R5bGU+PC9kZWZzPjx0aXRsZT5TY WZhcmlfU2hhcmU8L3RpdGxlPjxwb2x5bGluZSBjbGFzcz0iY2xzLTEiIHBvaW50cz0iMTMuMTMg OCAyMC4zOCA4IDIwLjM4IDI2Ljc1IDAuNSAyNi43NSAwLjUgOCA3LjUgOCIvPjxsaW5lIGNsYXN zPSJjbHMtMSIgeDE9IjEwLjQ0IiB5MT0iMTciIHgyPSIxMC40NCIvPjxsaW5lIGNsYXNzPSJjbH MtMSIgeDE9IjEwLjQ4IiB5MT0iMC4zOCIgeDI9IjE1LjI4IiB5Mj0iNS4xOCIvPjxsaW5lIGNsY XNzPSJjbHMtMSIgeDE9IjEwLjQ0IiB5MT0iMC4zOCIgeDI9IjUuNjQiIHkyPSI1LjE4Ii8+PC9z dmc+"> 
+      at the (top or bottom) of the browser.
+    </li>
+    <li>Then scroll (if needed) to find the "Add to Home Screen" button
+      <img height="22" alt="Add to Home Screen button Image" src="data:image/svg+xml;base64,PHN2ZyBpZD0iTGF5ZXJfMiIgZGF0YS1uYW1lPSJMYXllciAyIiB4bWxucz0iaHR0cDovL3d3dy5 3My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzNSAzNSI+PHRpdGxlPlNhZmFyaV9BMkhTPC 90aXRsZT48cmVjdCB3aWR0aD0iMzUiIGhlaWdodD0iMzUiIHJ4PSI4IiByeT0iOCIgc3R5bGU9I mZpbGw6IzhmOGY4ZiIvPjxsaW5lIHgxPSIyNC43NSIgeTE9IjE3LjUiIHgyPSIxMC4yNSIgeTI9 IjE3LjUiIHN0eWxlPSJmaWxsOm5vbmU7c3Ryb2tlOiNmZmY7c3Ryb2tlLXdpZHRoOjJweCIvPjx saW5lIHgxPSIxNy41IiB5MT0iMTAuMjUiIHgyPSIxNy41IiB5Mj0iMjQuNzUiIHN0eWxlPSJmaW xsOm5vbmU7c3Ryb2tlOiNmZmY7c3Ryb2tlLXdpZHRoOjJweCIvPjwvc3ZnPg==">
+    </li>
+    <li>Choose a name for the homescreen icon.</li>
+  </ol>
+</div>
+<script>
+let deferredPrompt;
+const log = (msg) => document.getElementById('txt-log').value += msg + '\r\n';
+const buttonInstall = document.getElementById('pwa-install');
+const showInstallPromotion = () => buttonInstall.style.display = 'flex';
+const hideInstallPromotion = () => buttonInstall.style.display = 'none';
+const isIOS = (/iPad|iPhone|iPod/.test(navigator.platform) ||
+              (navigator.platform === 'MacIntel' && 
+               navigator.maxTouchPoints > 1)) && !window.MSStream;
 
-
-
-/**
- * @constructor
- * @see chrome://serviceworker-internals/
- */
-reader.Worker = function() {
-  const /** string */ SERVICE_WORKER = '/sw.js';
-  const /** string */ INSTALL_PWA = 'pwa-install';
-
-  /**
-   * Initializes service worker.
-   * @private
-   */
-  const initServiceWorker_ = () => {
-    if ('serviceWorker' in dom.device) {
-      dom.device['serviceWorker']['register'](SERVICE_WORKER);
-    }
-  };
-
-  /**
-   * @private
-   */
-  const initInstallPrompt_ = () => {
-    dom.events.addEventListener(
-        dom.context, 'beforeinstallprompt', (event) => {
-          event.preventDefault();
-          installPromptEvent_ = event;
-          initInstallButton_(dom.getElementById(INSTALL_PWA));
-        });
-
-    // dom.events.addEventListener(
-    //     dom.context, 'appinstalled', function() {
-    //       console.log('onAppInstalled');
-    //     });
-  };
-
-  /**
-   * @param {?Element} element The install button container.
-   * @private
-   */
-  const initInstallButton_ = (element) => {
-    if (element) {
-      dom.css.addClass(element, 'visible');
-      dom.events.addEventListener(element, dom.events.TYPE.CLICK, () => {
-        dom.css.removeClass(element, 'visible');
-        installPromptEvent_.prompt();
-        installPromptEvent_['userChoice'].then((choice) => {
-          // console.log('User ' + choice.outcome + ' the A2HS prompt.');
-          installPromptEvent_ = dom.NULL;
-        });
-      });
-    }
-  };
-
-  /**
-   * Initializes service worker.
-   * @private
-   */
-  const init_ = () => {
-    if ('https:' == location.protocol) {
-      initServiceWorker_();
-      initInstallPrompt_();
-    }
-  };
-
-  /**
-   * @private ?Event
-   */
-  let installPromptEvent_;
-
-  // Initializing service worker.
-  init_();
+const init = () => {
+  if (isIOS) {
+    document.getElementById('ios-message').style.display = 'flex';
+  } else if (!window.BeforeInstallPromptEvent) {
+    document.getElementById('not-supported').style.display = 'flex';
+  } else {
+    navigator.serviceWorker.register('/sw.js');
+  }
 };
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  showInstallPromotion();
+});
+
+buttonInstall.addEventListener('click', async () => {
+  hideInstallPromotion();
+  deferredPrompt.prompt();
+  deferredPrompt = null;
+});
+
+window.addEventListener('appinstalled', () => {
+  hideInstallPromotion();
+  deferredPrompt = null;
+});
+
+function getPWADisplayMode() {
+  const isStandalone = window.matchMedia('(display-mode: standalone)').matches;
+  if (document.referrer.startsWith('android-app://')) {
+    return 'twa';
+  } else if (navigator.standalone || isStandalone) {
+    return 'standalone';
+  }
+  return 'browser';
+}
+
+init();
+</script>
+</body></html>
