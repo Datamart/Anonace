c32b1ce0412802efa1a267e61a6e967b3f215b19
Valentin Podkamennyi <vpodkamenyi@gmail.com>
2021-04-06 21:10:59 -0700

    Update sw.js

--- src/sw.js
+++ src/sw.js
@@ -152,7 +152,7 @@ const fetchAndCache_ = async (request) => {
 
   let response;
   try {
-    response = await fetch(request);
+    response = await fetch(request, {mode: 'no-cors'});
     if (isRequiestCacheble_(request) || isJsonpRequest) {
       const cache = await worker.caches.open(CACHE_KEY);
       if (JSONP_CACHE_KEY) {
